4959adead261916004468636decfa16d5f3ba9b9
felipehac <felipehasc@gmail.com>
2022-08-07 10:20:10 -0300

    feature: implemented a bunch of intents

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import fastify from 'fastify';
+import { load } from 'ts-dotenv';
+import { dialogFlowWebhookHandler } from './dialogflow/webhook-handler';
+import { createReadStream } from 'fs';
+import { runTests } from './pokemon/pokemon-repository';
+
+// runTests().then((_) => {
+//   process.exit(0);
+// });
+
+const env = load(
+  {
+    PORT: Number,
+  },
+  __dirname + '/../.env'
+);
+
+const server = fastify();
+
+server.get('/', (request, reply) => {
+  reply
+    .type('text/html')
+    .send(createReadStream(__dirname + '/../public/index.html'));
+});
+
+server.post('/chatbot-dialogflow-webhook', dialogFlowWebhookHandler);
+
+server.listen({ host: '0.0.0.0', port: env.PORT }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
